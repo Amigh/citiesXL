cbde56821e43d34f8c103987834c6d099cb87ec8
Amigh <ghafaria@ymail.com>
2015-08-18 14:58:02 +0430

    random image

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,6 +2,7 @@ class WelcomeController < ApplicationController
   before_action :authenticate_admin! , only: [:upload]
   def index
     @students = Student.limit(3).order('score DESC')
+    @background = rand(25).to_s + ".jpg"
   end
 
   def upload
