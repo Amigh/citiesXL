c1eb9c4e674221735fcd6937b899477de725306d
Amigh <ghafaria@ymail.com>
2015-11-26 10:59:49 +0330

    video added

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151123175136) do
+ActiveRecord::Schema.define(version: 20151126045952) do
 
   create_table "admins", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -91,6 +91,14 @@ ActiveRecord::Schema.define(version: 20151123175136) do
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
     t.string   "year"
+  end
+
+  create_table "tutorials", force: :cascade do |t|
+    t.string   "title"
+    t.text     "description"
+    t.string   "video"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
 end
