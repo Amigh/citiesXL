4e5e7e4a31e6278ef7d2a623d15bede518fbe021
Amigh <ghafaria@ymail.com>
2015-12-16 23:37:25 +0330

    bug

--- app/assets/javascripts/medals.js
+++ app/assets/javascripts/medals.js
@@ -4,7 +4,10 @@ set_positions = function(){
     // loop through and give each task a data-pos
     // attribute that holds its position in the DOM
     $('.scard').each(function(i){
-        $(this).attr("data-pos",18*($(this).data("page")-1) +i+1);
+        if ($(this).data("page") != '')
+            $(this).attr("data-pos",18*($(this).data("page")-1) +i+1);
+        else
+            $(this).attr("data-pos",i+1);
     });
 }
 
@@ -24,7 +27,10 @@ ready = function(){
 
         // populate the updated_order array with the new task positions
         $('.scard').each(function(i){
-            updated_order.push({ id: $(this).data("id"), position:18*($(this).data("page")-1) + i+1 });
+            if ($(this).data("page") != '')
+                updated_order.push({ id: $(this).data("id"), position:18*($(this).data("page")-1) + i+1 });
+            else
+                updated_order.push({ id: $(this).data("id"), position:i+1 });
         });
 
         // send the updated order via ajax
